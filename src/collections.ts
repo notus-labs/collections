bcb9f91bb1fe0955577ae5a09dd94f69408085b3
Omer GOKSOY <42944329+omergoksoy@users.noreply.github.com>
2024-12-11 19:22:26 +0300

    chore: nft collections add

--- src/collections.ts
+++ src/collections.ts
@@ -1,3 +1,129 @@
 import type { CollectionMetadata } from './types';
 
-export const COLLECTION_LIST: CollectionMetadata[] = [];
+export const COLLECTION_LIST: CollectionMetadata[] = [
+  {
+    tradeportId: '5fb36cd5-2527-44f7-9905-61096c442647',
+    structType:
+      '0x8f74a7d632191e29956df3843404f22d27bd84d92cca1b1abde621d033098769::rootlet::Rootlet',
+    title: 'Rootlets',
+    description:
+      'Cute creatures have come from the Suiverse to colonize Earth! Get ready, let’s root!',
+    website: 'https://rootlets.io',
+    createdAt: new Date('2024-08-13T01:17:43'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl:
+      'https://strapi-dev.scand.app/uploads/Rootlets_logo_2b348bf19d.jpg',
+  },
+  {
+    tradeportId: '67aca668-31b0-4713-91cf-079d392b041a',
+    structType:
+      '0x57191e5e5c41166b90a4b7811ad3ec7963708aa537a8438c1761a5d33e2155fd::kumo::Kumo',
+    title: 'Kumo',
+    description:
+      "Kumo is Lucky Kat Studios' cloud-cat mascot, bringing luck to the blockchain on Sui.",
+    website: 'https://kumothekat.com',
+    createdAt: new Date('2024-08-21T13:12:10'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: 'c3d1ce36-6afd-4336-b619-44c745dd7569',
+    structType:
+      '0x484932c474bf09f002b82e4a57206a6658a0ca6dbdb15896808dcd1929c77820::egg::AfEgg',
+    title: 'Egg',
+    description: '🥚',
+    website: 'https://aftermath.finance',
+    createdAt: new Date('2024-01-24T04:43:01'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: '07231735-96de-4710-8e11-52c61a482578',
+    structType:
+      '0x034c162f6b594cb5a1805264dd01ca5d80ce3eca6522e6ee37fd9ebfb9d3ddca::factory::PrimeMachin',
+    title: 'Prime Machin',
+    description: 'Prime Machin #333 manufactured by the Triangle Company.',
+    website: 'https://nozomi.world/collections/machin/prime/',
+    createdAt: new Date('2024-04-07T07:20:31'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: '4827d37b-5574-404f-b030-d26912ad7461',
+    structType:
+      '0xac176715abe5bcdaae627c5048958bbe320a8474f524674f3278e31af3c8b86b::fuddies::Fuddies',
+    title: 'Fuddies',
+    description: '10,000 extra special owls.',
+    website: 'https://fuddies.io/',
+    createdAt: new Date('2023-05-04T13:32:02'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: 'cb2e0bfd-84b1-41cb-b492-bf25078431f1',
+    structType:
+      '0x141d8a2333f9369452fe075331924bb98d2abf0ee98de941db85aaf809c4ef54::aeon::Aeon',
+    title: 'AEON',
+    description: 'https://suins.io/aeon',
+    website: 'A collection of 1000 unique, pixelated characters.',
+    createdAt: new Date('2024-10-24T11:57:02'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: '37569e2d-03e4-4bfb-ab55-0dae8943e6d3',
+    structType:
+      '0x862810efecf0296db2e9df3e075a7af8034ba374e73ff1098e88cc4bb7c15437::doubleup_citizens::DoubleUpCitizen',
+    title: 'DoubleUp Citizen',
+    description: 'A citizen on the DoubleUp Island!',
+    website: 'https://www.doubleup.fun/',
+    createdAt: new Date('2024-10-02T14:55:03'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: '9b2776c4-7e25-493a-a0bb-944ad4d23ad7',
+    structType:
+      '0x4125c462e4dc35631e7b31dc0c443930bd96fbd24858d8e772ff5b225c55a792::avatars::Avatar',
+    title: 'Cosmocadia',
+    description:
+      'Minted at the first Cosmocadia NFT launch. One of the pioneers.',
+    website: 'https://cosmocadia.io',
+    createdAt: new Date('2024-05-24T15:42:31'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: 'e5385a7a-075e-4382-96dd-1caaf960bd13',
+    structType:
+      '0x71ef69d02e77dff830c7de41db1452928c8ecd9040a541eef6729f139df83ffd::enforcer_machin_prototypes_01::Emp',
+    title: 'Enforcer Machin',
+    description:
+      'Enforcer Machin 973/3333 manufactured by the Triangle Company.',
+    website: 'https://twitter.com/_StudioMirai',
+    createdAt: new Date('2024-05-17T12:20:39'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+  {
+    tradeportId: '5830cc88-e420-43bb-a309-16b3ae2bf48d',
+    structType:
+      '0x4edaf43ada89b42ba4dee9fbf74a4dee3eb01f3cfd311d4fb2c6946f87952e51::dlab::Dlab',
+    title: 'DeSuiLabs',
+    description: '2222 Degenerates running experiments on the Blockchain.',
+    website: 'https://desuilabs.io/',
+    createdAt: new Date('2024-01-28T12:26:01'),
+    // selfReportedSupply: 0,
+    backgroundUrl: '',
+    iconUrl: '',
+  },
+];
